462c82be63579636186219797e48b8f1edc25e18
jalgelind <jalgelind@gmail.com>
2010-01-18 16:09:50 +0100

    Added basic abstract building blocks for the rendering system (Device, VertexBuffer).

---
+++ Include/Pxf/Graphics/Device.h
@@ -0,0 +1,24 @@
+#ifndef _PXF_GRAPHICS_DEVICE_H_
+#define _PXF_GRAPHICS_DEVICE_H_
+
+namespace Pxf
+{
+	class Window;
+	class WindowSpecifications;
+	
+	class VertexBuffer;
+	
+	//! Abstract video device
+	class Device
+	{
+		virtual Window* CreateWindow(WindowSpecifications* _pWindowSpecs) = 0;
+		virtual void DestroyWindow(Window* _pWindow) = 0;
+		
+		virtual VertexBuffer* CreateVertexBuffer() = 0;
+		virtual void DestroyVertexBuffer(VertexBuffer* _pVertexBuffer) = 0;
+		virtual void DrawVertexBuffer(VertexBuffer* _pVertexBuffer) = 0;
+		
+	};
+}
+
+#endif // _PXF_GRAPHICS_DEVICE_H_
---
+++ Include/Pxf/Graphics/VertexBuffer.h
@@ -0,0 +1,13 @@
+#ifndef _PXF_GRAPHICS_VERTEXBUFFER_H_
+#define _PXF_GRAPHICS_VERTEXBUFFER_H_
+
+namespace Pxf
+{
+	class VertexBuffer
+	{
+		private:
+		public:
+	};
+}
+
+#endif // _PXF_GRAPHICS_VERTEXBUFFER_H_
