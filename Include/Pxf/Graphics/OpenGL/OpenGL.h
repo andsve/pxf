03166f99b166a2e84f0a5660c60341c68aa39bd7
jalgelind <jalgelind@gmail.com>
2010-01-23 17:40:42 +0100

    Added OpenGL.h header that should work on all platforms.

---
+++ Include/Pxf/Graphics/OpenGL/OpenGL.h
@@ -0,0 +1,25 @@
+#ifndef _PXF_GRAPHICS_OPENGL_H_
+#define _PXF_GRAPHICS_OPENGL_H_
+
+#include <Pxf/Base/Config.h>
+
+#include <GL/glew.h>
+
+#if defined(CONF_FAMILY_WINDOWS)
+	#define WIN32_LEAN_AND_MEAN
+	#include <windows.h>
+	#undef CreateFont // ...
+#endif
+
+#if defined(CONF_PLATFORM_MACOSX)
+	#include <OpenGL/OpenGL.h>
+	#include <OpenGL/gl.h>
+	#include <OpenGL/glu.h>
+#else
+	#include <GL/gl.h>
+	#include <GL/glu.h>
+#endif
+
+#include <GL/glfw.h>
+
+#endif // _PXF_GRAPHICS_OPENGL_H_
