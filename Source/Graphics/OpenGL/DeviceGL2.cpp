b3de6189c9930b2666b0444c8e2914b9c3ed96d1
jalgelind <jalgelind@gmail.com>
2010-01-24 01:59:48 +0100

    Restored svens changes to DeviceGL2.

--- Source/Graphics/OpenGL/DeviceGL2.cpp
+++ Source/Graphics/OpenGL/DeviceGL2.cpp
@@ -3,12 +3,14 @@
 #include <Pxf/Graphics/QuadBatch.h>
 #include <Pxf/Graphics/OpenGL/VertexBufferGL.h>
 #include <Pxf/Graphics/OpenGL/DeviceGL2.h>
+#include <Pxf/Graphics/OpenGL/TextureGL2.h>
 #include <Pxf/Graphics/OpenGL/QuadBatchGL2.h>
 #include <Pxf/Graphics/PrimitiveType.h>
 #include <Pxf/Input/OpenGL/InputGL2.h>
 #include <Pxf/Base/Debug.h>
 
 #include <Pxf/Graphics/OpenGL/OpenGL.h>
+
 
 #define LOCAL_MSG "DeviceGL2"
 
@@ -56,12 +58,46 @@ void DeviceGL2::CloseWindow()
 	}
 }
 
+void DeviceGL2::SetViewport(int _x, int _y, int _w, int _h)
+{
+	glViewport(_x, _y, _w, _h);
+}
+
+void DeviceGL2::SetProjection(Math::Mat4 *_matrix)
+{
+	glMatrixMode (GL_PROJECTION);
+	glLoadMatrixf((GLfloat*)(_matrix->m));
+	glMatrixMode (GL_MODELVIEW);
+}
+
 void DeviceGL2::SwapBuffers()
 {
 	if (m_Window)
 	{
 		m_Window->Swap();
 	}
+}
+
+Texture* DeviceGL2::CreateTexture(const char* _filepath)
+{
+	TextureGL2* _tex = new TextureGL2();
+	_tex->Load(_filepath);
+	return _tex;
+}
+
+void DeviceGL2::BindTexture(Texture* _texture)
+{
+	glBindTexture(GL_TEXTURE_2D, ((TextureGL2*)_texture)->GetTextureID());
+}
+
+static GLuint _texture_units_array[16] = {GL_TEXTURE0, GL_TEXTURE1, GL_TEXTURE2, GL_TEXTURE3, GL_TEXTURE4,
+										GL_TEXTURE5, GL_TEXTURE6, GL_TEXTURE7, GL_TEXTURE8, GL_TEXTURE9,
+										GL_TEXTURE10, GL_TEXTURE11, GL_TEXTURE12, GL_TEXTURE13, GL_TEXTURE14,
+										GL_TEXTURE15};
+void DeviceGL2::BindTexture(Texture* _texture, unsigned int _texture_unit)
+{
+	glActiveTextureARB(_texture_units_array[_texture_unit]);
+	glBindTexture(GL_TEXTURE_2D, ((TextureGL2*)_texture)->GetTextureID());
 }
 
 
