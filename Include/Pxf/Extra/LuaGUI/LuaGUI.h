42c79858f729ccefd46db5d2b9ff591ebf99cd84
andsve <sweetfish@gmail.com>
2010-01-26 18:43:38 +0100

    Added Lua source.

---
+++ Include/Pxf/Extra/LuaGUI/LuaGUI.h
@@ -0,0 +1,23 @@
+#ifndef _PXF_EXTRA_LUAGUI_H_
+#define _PXF_EXTRA_LUAGUI_H_
+
+#include <Pxf/Base/Config.h>
+#include <Pxf/Base/Types.h>
+#include <Pxf/Base/ErrorType.h>
+
+namespace Pxf
+{
+
+	namespace Extra
+	{
+		class LuaGUI
+		{
+		public:
+			LuaGUI();
+			~LuaGUI();
+		};
+	}
+
+}
+
+#endif // _PXF_EXTRA_LUAGUI_H_
