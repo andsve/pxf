bf99074df2cbbd4ea8481cd53bae804cd50666f8
jalgelind <jalgelind@gmail.com>
2010-01-28 23:11:54 +0100

    Restored old Renderer-test.

--- Tests/Renderer/Source/Main.cpp
+++ Tests/Renderer/Source/Main.cpp
@@ -9,18 +9,12 @@
 #include <Pxf/Graphics/QuadBatch.h>
 #include <Pxf/Math/Math.h>
 #include <Pxf/Math/Matrix.h>
-#include <Pxf/Math/Vector.h>
 #include <Pxf/Input/Input.h>
 #include <Pxf/Util/String.h>
-#include <Pxf/Resource/ResourceManager.h>
 #include <Pxf/Resource/Image.h>
 #include <Pxf/Resource/Chunk.h>
 
-#include <Pxf/Graphics/InterleavedVertexBuffer.h>
-
 using namespace Pxf;
-using Math::Vec3f;
-using Math::Vec4f;
 
 bool PxfMain(Util::String _CmdLine)
 {
@@ -45,8 +39,7 @@ bool PxfMain(Util::String _CmdLine)
 	Graphics::Window* pWindow = pDevice->OpenWindow(pWindowSpecs);
 	Input::Input* pInput = engine.CreateInput(pDevice, pWindow);
 
-	Resource::ResourceManager* resourceManager = new Resource::ResourceManager();
-	Resource::Image* image = resourceManager->Acquire<Resource::Image>("test.png");
+	Pxf::Resource::Image t_Image(new Pxf::Resource::Chunk(),"test.png");
 
 	// Load some texture
 	glEnable(GL_TEXTURE_2D);
@@ -66,65 +59,8 @@ bool PxfMain(Util::String _CmdLine)
 
 	// Setup viewport and orthogonal projection
 	pDevice->SetViewport(0, 0, pWindowSpecs->Width / 2.0f, pWindowSpecs->Height);
-	Math::Mat4 t_ortho = Math::Mat4::Ortho(0, pWindowSpecs->Width / 2.0f, pWindowSpecs->Height, 0, -1000, 1000);
+	Math::Mat4 t_ortho = Math::Mat4::Ortho(0, pWindowSpecs->Width / 2.0f, pWindowSpecs->Height, 0, 0, 1);
 	pDevice->SetProjection(&t_ortho);
-
-	Graphics::InterleavedVertexBuffer* pBuff = pDevice->CreateInterleavedVertexBuffer(Graphics::VB_LOCATION_SYS);
-	
-	struct MyVertex
-	{
-		Vec3f v;
-		Vec4f c;
-		MyVertex(Vec3f _v, Vec4f _c)
-		{
-			v = _v;
-			c = _c;
-		}
-	};
-
-	
-	pBuff->CreateNewBuffer(24, sizeof(Vec3f) + sizeof(Vec4f), Graphics::VB_USAGE_STATIC_DRAW);
-
-	// SetData(Type, OffsetInBytes, NumComponents)
-	pBuff->SetData(Graphics::VB_VERTEX_DATA, 0, 3);
-	pBuff->SetData(Graphics::VB_COLOR_DATA, sizeof(Vec3f), 4);
-	pBuff->SetPrimitive(Graphics::VB_PRIMITIVE_QUADS);
-
-	MyVertex* data = (MyVertex*)pBuff->MapData(Graphics::VB_ACCESS_WRITE_ONLY);
-
-	// Front
-	data[0]  = MyVertex(Vec3f(-0.5f, -0.5f, 0.5f), Vec4f(0, 0, 1, 1.0f));
-	data[1]  = MyVertex(Vec3f(0.5f, -0.5f, 0.5f), Vec4f(0, 0, 1, 1.0f));
-	data[2]  = MyVertex(Vec3f(0.5f, 0.5f, 0.5f), Vec4f(0, 0, 1, 1.0f));
-	data[3]  = MyVertex(Vec3f(-0.5f, 0.5f, 0.5f), Vec4f(0, 0, 1, 1.0f));
-	// Back
-	data[4]  = MyVertex(Vec3f(-0.5f, -0.5f, -0.5f), Vec4f(1, 0, 1, 1.0f));
-	data[5]  = MyVertex(Vec3f(-0.5f, 0.5f, -0.5f), Vec4f(1, 0, 1, 1.0f));
-	data[6]  = MyVertex(Vec3f(0.5f, 0.5f, -0.5f), Vec4f(1, 0, 1, 1.0f));
-	data[7]  = MyVertex(Vec3f(0.5f, -0.5f, -0.5f), Vec4f(1, 0, 1, 1.0f));
-	// Top
-	data[8]  = MyVertex(Vec3f(-0.5f, 0.5f, 0.5f), Vec4f(0, 1, 1, 1.0f));
-	data[9]  = MyVertex(Vec3f(0.5f, 0.5f, 0.5f), Vec4f(0, 1, 1, 1.0f));
-	data[10] = MyVertex(Vec3f(0.5f, 0.5f, -0.5), Vec4f(0, 1, 1, 1.0f));
-	data[11] = MyVertex(Vec3f(-0.5f, 0.5f, -0.5f), Vec4f(0, 1, 1, 1.0f));
-	// Left
-	data[12] = MyVertex(Vec3f(-0.5f, -0.5f, 0.5f), Vec4f(1, 1, 1, 1.0f));
-	data[13] = MyVertex(Vec3f(-0.5f, 0.5f, 0.5f), Vec4f(1, 1, 1, 1.0f));
-	data[14] = MyVertex(Vec3f(-0.5f, 0.5f, -0.5f), Vec4f(1, 1, 1, 1.0f));
-	data[15] = MyVertex(Vec3f(-0.5f, -0.5f, -0.5f), Vec4f(1, 1, 1, 1.0f));
-	// Right
-	data[16] = MyVertex(Vec3f(0.5f, -0.5f, -0.5f), Vec4f(1, 0, 1, 1.0f));
-	data[17] = MyVertex(Vec3f(0.5f, 0.5f, -0.5f), Vec4f(1, 0, 1, 1.0f));
-	data[18] = MyVertex(Vec3f(0.5f, 0.5f, 0.5f), Vec4f(1, 0, 1, 1.0f));
-	data[19] = MyVertex(Vec3f(0.5f, -0.5f, 0.5f), Vec4f(1, 0, 1, 1.0f));
-	// Bottom
-	data[20] = MyVertex(Vec3f(-0.5f, -0.5f, 0.5f), Vec4f(0, 1, 1, 1.0f));
-	data[21] = MyVertex(Vec3f(-0.5f, -0.5f, -0.5f), Vec4f(0, 1, 1, 1.0f));
-	data[22] = MyVertex(Vec3f(0.5f, -0.5f, -0.5f), Vec4f(0, 1, 1, 1.0f));
-	data[23] = MyVertex(Vec3f(0.5f, -0.5f, 0.5f), Vec4f(0, 1, 1, 1.0f));
-
-	pBuff->UnmapData();
-
 
 	float t_honk = 0.0f;
 	while (!pInput->IsKeyDown(Input::ESC) && pWindow->IsOpen())
@@ -137,19 +73,13 @@ bool PxfMain(Util::String _CmdLine)
 			         .3, .3, 0);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glLoadIdentity();
+		glTranslatef(0.375, 0.375, 0);
 
-		t_honk += 0.004;
-
-		glEnable(GL_DEPTH_TEST);
-		glBindTexture(GL_TEXTURE_2D, 0);
-		glTranslatef(100, 100, 0);
-		glRotatef(t_honk*80, 0, 1, 1);
-		glScalef(50, 50, 50);
-
-		pDevice->DrawBuffer(pBuff);
-
-		//pDevice->BindTexture(pTexture);
-		//pQBatch->Draw();
+		t_honk += 0.001f;
+		glTranslatef(cosf(t_honk) * 200.0f, sinf(t_honk) * 200.0f, 0);
+		
+		pDevice->BindTexture(pTexture);
+		pQBatch->Draw();
 
 		// Swap buffers
 		pWindow->Swap();
