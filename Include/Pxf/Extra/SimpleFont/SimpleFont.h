bf7741efaf1b6675ec369c9eaea08875466f9937
sweetfish <sweetfish@gmail.com>
2010-02-06 17:03:03 +0100

    Started work on a SimpleFont class that should utilize stb_truetype.

---
+++ Include/Pxf/Extra/SimpleFont/SimpleFont.h
@@ -0,0 +1,20 @@
+#ifndef _PXF_EXTRA_SIMPLEFONT_H_
+#define _PXF_EXTRA_SIMPLEFONT_H_
+
+namespace Pxf
+{
+	namespace Extra
+	{
+		class SimpleFont
+		{
+		public:
+			SimpleFont ();
+			virtual ~SimpleFont ();
+
+		private:
+			
+		};
+	} /* Extra */
+} /* Pxf */
+
+#endif
