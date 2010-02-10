a0da0ed003fc31f3a359d5e9128defef8750f0b3
jalgelind <jalgelind@gmail.com>
2010-02-11 00:42:40 +0100

    Added DeviceResource.h, added implementation of Timer.

--- Include/Pxf/Base/Timer.h
+++ Include/Pxf/Base/Timer.h
@@ -5,42 +5,17 @@
 
 namespace Pxf
 {
-	inline uint64 RDTSC(void)
-	{
-	  _asm  _emit 0x0F
-	  _asm  _emit 0x31
-	}
-
-	//! Abstract class used for timers
 	class Timer
 	{
-	public:
-		virtual inline void Start() = pure;
-		virtual inline void Stop()	= pure;
-		virtual uint64 Interval() = pure;
-	}; // Timer
-
-	class PCTimer : Timer
-	{
 	private:
-		uint64 m_StartTime;
-		uint64 m_StopTime;
+		uint64 m_Start;
+		uint64 m_Stop;
 	public:
-		inline void Start();
-		inline void Stop();
+		Timer();
+		void Start();
+		void Stop();
 		uint64 Interval();
-	}; // PerformanceCounter
-
-	class RDTSCTimer : Timer
-	{
-	private:
-		uint64 m_StartCycle;
-		uint64 m_EndCycle;
-	public:
-		inline void Start();
-		inline void Stop();
-		uint64 Interval();
-	}; // RDTSCTimer
+	};
 }
 
 #endif //__PXF_BASE_TIMER_H__
