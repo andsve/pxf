e278d465497d202cdb919c6b4de77e5138b9e0a4
sweetfish <sweetfish@gmail.com>
2010-06-24 20:30:59 +0200

    Added skeleton of LuaGame

--- Include/Pxf/Extra/LuaGame/LuaGame.h
+++ Include/Pxf/Extra/LuaGame/LuaGame.h
@@ -1,2 +1,31 @@
-// empty
+#ifndef __PXF_EXTRA_LUAGAME_LUAGAME_H__
+#define __PXF_EXTRA_LUAGAME_LUAGAME_H__
 
+#include <Pxf/Util/String.h>
+
+namespace Pxf
+{
+    
+    namespace Extra
+    {
+        
+        class LuaGame
+        {
+        public:
+            LuaGame (Util::String _gameFilename);
+            ~LuaGame ();
+            
+            bool PreLoad();
+            bool Update(float dt);
+            bool Render();
+
+        private:
+            Util::String m_GameFilename;
+            Util::String m_GameIdent;
+            int m_GameVersion[3]; // Simple mechanism for version handling 1.2.0 -> [0].[1].[2]
+        };
+        
+    } /* Extra */
+} /* Pxf */
+
+#endif // __PXF_EXTRA_LUAGAME_LUAGAME_H__
--- Source/Extra/LuaGame/LuaGame.cpp
+++ Source/Extra/LuaGame/LuaGame.cpp
@@ -1,2 +1,38 @@
-// empty
+
+#include <Pxf/Extra/LuaGame/LuaGame.h>
+
+using namespace Pxf;
+using namespace Pxf::Extra;
+
+LuaGame::LuaGame(Util::String _gameFilename)
+{
+    m_GameFilename = _gameFilename;
+}
+
+LuaGame::~LuaGame()
+{
+    // Destructor of LuaGame
+    // Do nothing?
+}
+
+bool LuaGame::PreLoad()
+{
+    // Preload game data
+    return true;
+}
+
+bool LuaGame::Update(float dt)
+{
+    // Update game
+    
+    return true;
+}
+
+bool LuaGame::Render()
+{
+    // Render game
+    
+    return true;
+}
+
 
