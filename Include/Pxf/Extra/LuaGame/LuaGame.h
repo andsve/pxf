c1a1bb0eb92904dcf63c56527beef1f9e0d82923
sweetfish <sweetfish@gmail.com>
2010-06-24 21:17:48 +0200

    Started adding lua base into LuaGame

--- Include/Pxf/Extra/LuaGame/LuaGame.h
+++ Include/Pxf/Extra/LuaGame/LuaGame.h
@@ -2,27 +2,44 @@
 #define __PXF_EXTRA_LUAGAME_LUAGAME_H__
 
 #include <Pxf/Util/String.h>
+#include <Pxf/Graphics/Device.h>
+
+// Lua includes
+extern "C" {
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+}
 
 namespace Pxf
 {
     
     namespace Extra
     {
-        
         class LuaGame
         {
         public:
-            LuaGame (Util::String _gameFilename);
-            ~LuaGame ();
+            LuaGame(Util::String _gameFilename, Graphics::Device* _device);
+            ~LuaGame();
             
-            bool PreLoad();
+            bool Load();
+            int  PreLoad();
             bool Update(float dt);
             bool Render();
 
         private:
+            bool m_Running; // Script state
+            
             Util::String m_GameFilename;
             Util::String m_GameIdent;
-            int m_GameVersion[3]; // Simple mechanism for version handling 1.2.0 -> [0].[1].[2]
+            int m_GameVersion[3]; // Simple mechanism for version handling 1.2.3 -> [0].[1].[2]
+            lua_State *L;
+            
+            // Pxf device
+            Graphics::Device* m_Device;
+            
+            // Private methods
+            void _register_lua_libs_callbacks();
         };
         
     } /* Extra */
--- Source/Extra/LuaGame/LuaGame.cpp
+++ Source/Extra/LuaGame/LuaGame.cpp
@@ -1,12 +1,24 @@
 
 #include <Pxf/Extra/LuaGame/LuaGame.h>
 
+#define LOCAL_MSG "LuaGame"
+
 using namespace Pxf;
 using namespace Pxf::Extra;
 
-LuaGame::LuaGame(Util::String _gameFilename)
+LuaGame::LuaGame(Util::String _gameFilename, Graphics::Device* _device)
 {
+    m_Device = _device;
+    
     m_GameFilename = _gameFilename;
+    m_GameIdent = "Unknown Game";
+    
+    // Reset version
+    m_GameVersion[0] = 0;
+    m_GameVersion[1] = 0;
+    m_GameVersion[2] = 0;
+    
+    m_Running = false;
 }
 
 LuaGame::~LuaGame()
@@ -15,10 +27,51 @@ LuaGame::~LuaGame()
     // Do nothing?
 }
 
-bool LuaGame::PreLoad()
+bool LuaGame::Load()
+{   
+    // Init lua state
+    L = lua_open();
+    
+    // Register lua libs
+    _register_lua_libs_callbacks();
+    
+    // Load game script   
+    int s = luaL_loadfile(L, m_GameFilename.c_str());
+	if (!s)
+	{
+		s = lua_pcall(L, 0, LUA_MULTRET, 0);
+
+		if ( s ) {
+			Message(LOCAL_MSG, "-- %s", lua_tostring(L, -1));
+			lua_pop(L, 1); // remove error message
+		} else {
+			m_Running = true;
+						
+			// Load font file
+			/*lua_getglobal(L, "theme_font_file");
+			lua_getglobal(L, "theme_font_size");
+			m_Font->Load(lua_tostring(L, 1), lua_tonumber(L, 2));
+			//m_Texture = m_Device->CreateTexture(lua_tostring(L, 1));
+			lua_pop(L, 2);
+			*/
+
+			// Call init()
+			//CallLuaFunc("init");
+		}
+
+	} else {
+		Message(LOCAL_MSG, "Error: %s",lua_tostring(L,-1));
+	}
+    
+    return true;
+}
+
+int LuaGame::PreLoad()
 {
     // Preload game data
-    return true;
+    
+    // Returns how many preload data pieces there are left
+    return 0;
 }
 
 bool LuaGame::Update(float dt)
@@ -35,4 +88,25 @@ bool LuaGame::Render()
     return true;
 }
 
+void LuaGame::_register_lua_libs_callbacks()
+{
+	// Lua libs
+	static const luaL_Reg lualibs[] = {
+		{"", luaopen_base},
+		{LUA_LOADLIBNAME, luaopen_package},
+		{LUA_TABLIBNAME, luaopen_table},
+		//{LUA_IOLIBNAME, luaopen_io},
+		//{LUA_OSLIBNAME, luaopen_os},
+		{LUA_STRLIBNAME, luaopen_string},
+		{LUA_MATHLIBNAME, luaopen_math},
+		{LUA_DBLIBNAME, luaopen_debug},
+		{NULL, NULL}
+	};
+
+	for (const luaL_Reg *lib = lualibs; lib->func; lib++) {
+		lua_pushcfunction(L, lib->func);
+		lua_pushstring(L, lib->name);
+		lua_call(L, 1, 0);
+	}
+}
 
