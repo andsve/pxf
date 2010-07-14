e35e970b9a41bb4c4cb9d02d4678e82f1cbd3cce
Jhonnyg <jhonny.goransson@gmail.com>
2010-07-14 15:14:24 +0200

    Physics Subsystem

--- Include/Pxf/Extra/LuaGame/Subsystems/Physics.h
+++ Include/Pxf/Extra/LuaGame/Subsystems/Physics.h
@@ -2,6 +2,11 @@
 #define __PXF_EXTRA_LUAGAME_SUBSYSTEMS_PHYSICS_H__
 
 #include <Pxf/Extra/LuaGame/Subsystem.h>
+#include <Pxf/Math/Vector.h>
+
+// box2D forward declarations
+class b2World;
+class b2Body;
 
 namespace Pxf
 {
@@ -13,6 +18,59 @@ namespace Pxf
             {
                 void RegisterClass(lua_State* _L);
                 int NewWorld(lua_State* _L);
+
+				// helpers
+				int _NewBox2DWorld(lua_State* _L);
+				int _NewBullet3DWorld(lua_State* _L);
+
+				class LuaPhysicsBody : public Subsystem
+				{
+				public:
+					//virtual void RegisterClass(lua_State* _L) = 0;
+					virtual int GetPosition(lua_State* _L) = 0; 
+				private:
+				};
+
+				class LuaBox2DPhysicsBody : public LuaPhysicsBody
+				{
+				public:
+					int GetPosition(lua_State* _L);
+				private:
+					b2Body* m_Body;
+				};
+
+				class LuaPhysicsWorld : Subsystem
+				{
+				public:
+					LuaPhysicsWorld(Math::Vec3f _Gravity) { 
+						m_Gravity = _Gravity;
+					}
+
+					void RegisterClass(lua_State* _L);
+
+					virtual int NewBody(lua_State* _L) = 0;
+					virtual int GetType() = 0;
+
+					Math::Vec3f GetGravity() { return m_Gravity; }
+				private:
+					Math::Vec3f m_Gravity;
+				};
+
+				enum LuaPhysicsWorldType {
+					WORLD_TYPE_BULLET3D = 1,
+					WORLD_TYPE_BOX2D = 2
+				};
+
+				class LuaBox2DPhysicsWorld : public LuaPhysicsWorld
+				{
+				public:
+					LuaBox2DPhysicsWorld(Math::Vec3f _Gravity) 
+						: LuaPhysicsWorld(_Gravity) { }
+					int NewBody(lua_State* _L);
+					int GetType() { return WORLD_TYPE_BOX2D; }
+				private:
+					b2World* m_World;
+				};
             } /* PhysicsSubsystem */
         } /* LuaGame */
     } /* Extra */
--- Source/Extra/LuaGame/Subsystems/Physics.cpp
+++ Source/Extra/LuaGame/Subsystems/Physics.cpp
@@ -3,6 +3,7 @@
 
 using namespace Pxf;
 using namespace Extra::LuaGame;
+using namespace Extra::LuaGame::PhysicsSubsystem;
 
 void PhysicsSubsystem::RegisterClass(lua_State* _L)
 {
@@ -58,6 +59,8 @@ void PhysicsSubsystem::RegisterClass(lua_State* _L)
 
 int PhysicsSubsystem::NewWorld(lua_State* _L)
 {
+	int _RetVal = 0;
+
 	int argc = lua_gettop(_L);	
 	if(argc == 1 && lua_isnumber(_L,1))
 	{
@@ -65,13 +68,15 @@ int PhysicsSubsystem::NewWorld(lua_State* _L)
 		
 		switch(_Val)
 		{
-			case 1: 
-				printf("luls\n"); 
+			case 1: // Bullet3D
 				break;
-			case 2:
-				printf("bruls\n");
+			case 2: // Box2D
+				_RetVal = _NewBox2DWorld(_L);
 				break;
-			default: break;
+			default: 
+				lua_pushstring(_L,"Unknown physics identifier passed to newworld function");
+				lua_error(_L);
+				break;
 		}
 	}
 	else
@@ -80,5 +85,63 @@ int PhysicsSubsystem::NewWorld(lua_State* _L)
 		lua_error(_L);
 	}
 	
+	return _RetVal;
+}
+
+int PhysicsSubsystem::_NewBox2DWorld(lua_State* _L)
+{
+	LuaPhysicsWorld* _NewWorld = new LuaBox2DPhysicsWorld(Math::Vec3f(0.0f,0.0f,0.0f));
+
+	lua_newtable(_L);
+    
+    lua_pushlightuserdata(_L, _NewWorld);
+    lua_setfield(_L, -2, "instance");
+
+//	lua_pushcfunction(_L, lol);
+//    lua_setfield(_L, -2, "lol");
+
+	return 1;
+}
+
+void LuaPhysicsWorld::RegisterClass(lua_State* _L)
+{
+
+
+}
+
+int LuaBox2DPhysicsBody::GetPosition(lua_State* _L) 
+{
+
 	return 0;
 }
+
+int LuaBox2DPhysicsWorld::NewBody(lua_State* _L) 
+{
+	LuaBox2DPhysicsBody* _NewBody = new LuaBox2DPhysicsBody();
+
+	lua_newtable(_L);
+    
+    lua_pushlightuserdata(_L, _NewBody);
+    lua_setfield(_L, -2, "instance");
+
+	//lua_pushcfunction(_L, _NewBody->GetPosition);
+	//lua_setfield(_L, -2, "position");
+
+	return 1;
+}
+
+/*
+void LuaPhysicsBody::RegisterClass(lua_State* _L) 
+{
+	// luagame.physics.body
+	/*
+	lua_getglobal(_L, LUAGAME_TABLE);
+	lua_getfield(_L, -1, "physics");
+	lua_newtable(_L);
+	lua_setfield(_L, -2, "body");
+	lua_getfield(_L, -1, "body");
+
+	// luagame.physics.body.position 
+	lua_pushcfunction(_L,GetPosition);
+	//lua_setfield(_L, -2, "position"); 
+}*/
